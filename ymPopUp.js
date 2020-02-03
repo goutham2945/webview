bbb9691845b39110af7d40c011f43da316cb9a21
GouthamRaju <gouthamcdp@gmail.com>
2020-02-03 12:17:41 +0530

    Create ymPopUp.js

---
+++ ymPopUp.js
@@ -0,0 +1,22 @@
+(function () {
+    window.addEventListener("message", function (event) {
+        try {
+            event = JSON.parse(event.data);
+        } catch (error) {
+            event = {}
+        }
+        if (event.event_code === 'custom-event') {
+            iframe = document.getElementById('ymIframeId');
+            var eventData = event.data.data;
+            console.log(eventData);
+            switch (event.data.code) {
+                case "login":
+                    var myWindow = window.open(eventData.url, "new window", "height=500, width=500, top=400, left=400");
+                    return;
+                default:
+                    console.log('No event handler defined for ' + event.data.code);
+                    return;
+            }
+        }
+    }, false);
+})();
